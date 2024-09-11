7b1f0c6849a32b4bd38c50d6e0f9f280aa5e4f38
Валуев Егор <163563205+m0r1d@users.noreply.github.com>
2024-09-11 20:19:36 +0300

    Add files via upload

--- Hw_2.c
+++ Hw_2.c
@@ -8,6 +8,6 @@ m = 3;
 int main()
 {
 	setlocale(LC_ALL, "RUS");
-	printf("Р”Р°РЅРѕ:\n %14g\n %14g\n %20s\n РћС‚РІРµС‚:\n  %+20f", n, L, "_________", n/L);
+	printf("Дано:\n %14g\n %14g\n %20s\n Ответ:\n  %+20f", n, L, "_________", n/L);
 	getchar();
 }
