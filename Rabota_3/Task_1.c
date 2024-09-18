09256046522ffce5c5e6daf7679e75d0eb304d48
Валуев Егор <163563205+m0r1d@users.noreply.github.com>
2024-09-18 19:09:32 +0300

    rabota3

---
+++ Rabota_3/Task_1.c
@@ -0,0 +1,22 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<locale.h>
+//void main()
+//{
+//	int num;
+//	int num2;
+//	setlocale(LC_ALL, "RUS");
+//	puts("¬ведите число:");
+//	scanf("%d", &num);
+//	puts("¬ведите второе число");
+//	scanf("%d", &num2);
+//	getchar();
+//	puts("результаты вычислений:");
+//	printf("%d-%d=%d\n", num, num2, num-num2);
+//	printf("%d+%d=%d\n", num, num2, num + num2);
+//	printf("%d*%d=%d\n", num, num2, num * num2);
+//	printf("%d/%d=%d\n", num, num2, num / num2);
+//	printf("%d//%d=%d\n", num, num2, num % num2);
+//	getchar();
+//	return 0;
+//}
---
+++ Rabota_3/Task_2.c
@@ -0,0 +1,21 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<locale.h>
+//#define      D       2.54
+//#define      ID       2.32166
+//
+//void main()
+//{
+//	int dym;
+//	float result;
+//	float result2;
+//	setlocale(LC_ALL, "RUS");
+//	puts("Введите величину в дюймах:");
+//	scanf("%d", &dym);
+//	result=D*dym;
+//	result2=ID*dym;
+//	getchar();
+//	printf("%d Английских дюймов – это %.1f Сантимеров\n", dym, result);
+//	printf("%d Испанских дюймов – это %.1f Сантимеров", dym, result2);
+//	getchar();
+//}
