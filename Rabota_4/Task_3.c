ffe8f5bc2298659b0c93b8ee1bad56ca26b563a5
Валуев Егор <163563205+m0r1d@users.noreply.github.com>
2024-09-25 00:06:15 +0300

    Rabota_4

---
+++ Rabota_4/Task_3.c
@@ -0,0 +1,20 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<locale.h>
+//void main()
+//{
+//	int n;
+//	int n1;
+//	int n2;
+//	int n3;
+//	setlocale(LC_ALL, "RUS");
+//	puts("Введите число:");
+//	scanf("%d",&n);
+//	n1 = n / 100;
+//	n2 = (n-(n1*100))/10;
+//	n3 = n % 10;
+//	printf("Последняя цифра %d , Первая цифра %d ,Сумма цифр %d\n",n3,n1,n1+n2+n3);
+//	getchar();
+//	printf("Число Наоборот %d%d%d",n3,n2,n1);
+//	getchar();
+//}
