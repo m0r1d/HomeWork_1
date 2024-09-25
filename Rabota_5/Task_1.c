618d9fbaea37bc585050fb2b908f42208dc8cf52
Валуев Егор <163563205+m0r1d@users.noreply.github.com>
2024-09-25 10:02:10 +0300

    Add files via upload

---
+++ Rabota_5/Task_1.c
@@ -0,0 +1,18 @@
+//#define _USE_MATH_DEFINES
+//#define M_PI 3.14159265358979323846
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<locale.h>
+//#include<math.h>
+//#include<stdlib.h>
+//void main()
+//{
+//	setlocale(LC_ALL, "RUS");
+//	double gr,R;
+//	puts("Введите число градусов");
+//	scanf("%lg",&gr);
+//	R=gr*M_PI/180;
+//	R=sin(R);
+//	printf("Синус угла %lg равен %.6lg",gr,R);
+//
+//}
