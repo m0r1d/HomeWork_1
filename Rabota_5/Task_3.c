6426c82d4c91b05c126a0e47ad93897697a21ebe
Валуев Егор <163563205+m0r1d@users.noreply.github.com>
2024-09-25 11:38:22 +0300

    Add files via upload

---
+++ Rabota_5/Task_3.c
@@ -0,0 +1,34 @@
+//#define _USE_MATH_DEFINES
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<locale.h>
+//#include<math.h>
+//#include<stdlib.h>
+//void main()
+//{
+//	setlocale(LC_ALL, "RUS");
+//	float t, x, b, y, a;
+//	puts("Введите значение x\n");
+//	scanf("%f",&x);
+//	t = 0.3;
+//	b = x *(pow(t,2)+(pow(x,1./2)));
+//	a = log10(fabs(t* x+(pow(t,2))));
+//	y = log((pow(a,2))/(a+b));
+//	printf("Ответ %.2f\n",y);
+//	int ys1,ys2;
+//	int a1,b1,c1;
+//	a1 = (int)a;
+//	b1 = (int)b;
+//	c1 = (int)y;
+//	ys1 = (a1%2==0)^(b1%2==0);
+//	ys2 = (a1%3==0) && (b1%3==0) && (c1%3==0);
+//	printf("Первое условие: %i\n",ys1);
+//	printf("Второе условие: %i\n", ys2);
+//	printf("Условия выполнены: %i\n", ys1 * ys2);
+//
+//
+//
+//
+//
+//
+//}
