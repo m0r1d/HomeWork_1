b46af02dc4ac9661362b1e3695c96523356c748b
Валуев Егор <163563205+m0r1d@users.noreply.github.com>
2024-10-03 11:24:27 +0300

    Rabota_7

---
+++ Rabota_7/Task_7_1.c
@@ -0,0 +1,47 @@
+//
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<locale.h>
+//
+//void main()
+//{
+//	setlocale(LC_ALL, "RUS");
+//	char c;
+//	printf("Введите букву или цифру:");
+//	scanf("%c", &c);
+//	switch (c)
+//	{case '1':
+//		printf("Введена цифра.\n");
+//		break;
+//	case '2':
+//		printf("Введена цифра.\n");
+//		break;
+//	case '3':
+//		printf("Введена цифра.\n");
+//		break;
+//	case '4':
+//		printf("Введена цифра.\n");
+//		break;
+//	case '5':
+//		printf("Введена цифра.\n");
+//		break;
+//	case '6':
+//		printf("Введена цифра.\n");
+//		break;
+//	case '7':
+//		printf("Введена цифра.\n");
+//		break;
+//	case '8':
+//		printf("Введена цифра.\n");
+//		break;
+//	case '9':
+//		printf("Введена цифра.\n");
+//		break;
+//	case '0':
+//		printf("Введена цифра.\n");
+//		break;
+//	default:
+//		printf("Введена буква\n");
+//	}
+//	return 0;
+//}
---
+++ Rabota_7/Task_7_2.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<locale.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	float x,y;
+	char c;
+	puts("¬ведите выражение:");
+	scanf("%f %c %f",&x, &c, &y);
+	switch (c)
+	{
+	case '+':
+		printf("=%f",x+y);
+		break;
+	case '-':
+		printf("=%f", x - y);
+		break;
+	case '*':
+		printf("=%f", x * y);
+		break;
+	case '/':
+		printf("=%f", x / y);
+		break;
+	case '^':
+		printf("=%f", pow(x,y));
+		break;
+	default:
+		printf("неизвестна€ операци€");
+		break;
+
+	}
+	return 0;
+}
