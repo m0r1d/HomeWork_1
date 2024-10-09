e9b0dc643076928fb15d6affd5611ae9f8bdd70e
Валуев Егор <163563205+m0r1d@users.noreply.github.com>
2024-10-09 11:35:04 +0300

    Add files via upload

---
+++ pokaz_NEW/Task_8_0.c
@@ -0,0 +1,6 @@
+//void main()
+//{
+//	for (int start = 1; start <= 5; start++) putchar('*');
+//	for (int start = 5; start <= 10; start++) putchar('-');
+//	puts("+");
+//}
---
+++ pokaz_NEW/Task_8_1.c
@@ -0,0 +1,20 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include<locale.h>
+//
+//int main() {
+//    setlocale(LC_ALL, "RUS");
+//    int n, m; 
+//    int s = 0; 
+//    int k = 0; 
+//    printf("¬ведите значение m: ");
+//    scanf("%d", &m);
+//    printf("¬ведите значение n: ");
+//    scanf("%d", &n);
+//    for (int i = m; i >= n; i--) { 
+//        s += i; 
+//        printf("выполнено %d раз\n", ++k); 
+//    }
+//    printf("результат: %d\n", s);
+//    return 0;
+//}
