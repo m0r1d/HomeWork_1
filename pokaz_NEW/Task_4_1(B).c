23598a44e90cf6bdfdb0da1e54707d881477518e
Валуев Егор <163563205+m0r1d@users.noreply.github.com>
2024-10-09 10:09:38 +0300

    Add files via upload

---
+++ pokaz_NEW/Task_4_1(B).c
@@ -0,0 +1,16 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<locale.h>
+//void main()
+//{
+//	int i;
+//	setlocale(LC_ALL, "RUS");
+//	puts("¬ведите число которое хотите преобразовать:");
+//	scanf("%d", &i);
+//	getchar();
+//	printf("%i %x",i,i);
+//	getchar();
+//
+//
+//
+//}
---
+++ pokaz_NEW/Task_6_2 (2).c
@@ -0,0 +1,13 @@
+//#define _USE_MATH_DEFINES
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<locale.h>
+//#include<math.h>
+//#include<stdlib.h>
+//void main()
+//{
+//	setlocale(LC_ALL, "RUS");
+//
+//
+//
+//}
