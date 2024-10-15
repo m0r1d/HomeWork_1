46de803475fb62bb38f6b3408278a37f6e781445
Валуев Егор <163563205+m0r1d@users.noreply.github.com>
2024-10-16 00:18:32 +0300

    pokaz_v3

---
+++ pokaz_V3/Task_4_1_1.c
@@ -0,0 +1,17 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<locale.h>
+//void main()
+//{
+//	float i;
+//	int c;
+//	float d;
+//	setlocale(LC_ALL, "RUS");
+//	puts("Введите число которое хотите преобразовать:");
+//	scanf("%f", &i);
+//	getchar();
+//	c = (int)i;
+//	d = i - c; 
+//	printf("А)  %d %f\n", c, d);
+//	getchar();
+//}
---
+++ pokaz_V3/Task_4_1_3.c
@@ -0,0 +1,15 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<locale.h>
+//void main()
+//{
+//	float i;
+//	setlocale(LC_ALL, "RUS");
+//	puts("Введите число которое хотите преобразовать:");
+//	scanf("%f", &i);
+//	getchar();
+//	printf("В)  %f\n", 1 / i);
+//	getchar();
+//
+//
+//}
