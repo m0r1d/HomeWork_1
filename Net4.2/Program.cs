586ff56de4ed660e0929774ad5a4ac1942f5973b
Валуев Егор <163563205+m0r1d@users.noreply.github.com>
2025-10-28 23:47:00 +0300

    Add files via upload

---
+++ Net4.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+using System.Windows.Forms;
+
+namespace MultiArrayExample
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
